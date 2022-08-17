7eaab10d98d719ae9c18ec5958fac6f3f59b150d
RafiTahmid <rafi.tahmid019@gmail.com>
2022-08-17 21:48:44 +0600

    bank html added

--- js/login.js
+++ js/login.js
@@ -1,3 +1,14 @@
 document. getElementById('btn-submit'). addEventListener('click', function(){
-    console.log('Connected')
+    const emailField = document. getElementById('user-email');
+    const email = emailField.value;    
+    
+    const passwordField = document. getElementById('user-password');
+    const password = passwordField. value;
+
+    if (email === 'pola@baap.com' && password === 'secret'){
+        window. location. href = 'bank.html'
+    }
+    else{
+        alert('Akash vora tara');
+    }
 })
